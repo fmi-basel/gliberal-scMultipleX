3f28703c119889d3b66eb8190d41135fb2a79937
Enrico Tagliavini <enrico.tagliavini@fmi.ch>
2023-03-03 11:57:30 +0100

    migrate 06_tidy_membrane_features.py to new config parser
    and rename a few parameters for consistency

--- scripts/prefect/06_tidy_membrane_features.py
+++ scripts/prefect/06_tidy_membrane_features.py
@@ -6,13 +6,18 @@ from prefect import Flow, Parameter, task
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import LocalRun
 
+from scmultiplex.config import (
+    compute_workflow_params,
+    get_round_names,
+    summary_csv_path,
+)
 from scmultiplex.utils.accumulate_utils import save_tidy_plate_well_org_mem
 
 
 @task()
-def load_experiment(exp_csv):
+def load_experiment(exp_path):
     exp = Experiment()
-    exp.load(exp_csv)
+    exp.load(exp_path)
     return exp
 
 
@@ -26,17 +31,29 @@ with Flow(
     executor=LocalDaskExecutor(),
     run_config=LocalRun(),
 ) as flow:
-    exp_csv = Parameter("exp_csv", default="/path/to/exp/summary.csv")
+    exp_path = Parameter("exp_path", default="/path/to/exp/summary.csv")
 
-    exps = load_experiment(exp_csv)
+    exps = load_experiment(exp_path)
 
     save_org = save_tidy_plate_well_org_mem_task(exps)
 
 
-def conf_to_dict(config):
-    return {
-        "exp_csv": config["DEFAULT"]["exp_csv"],
-    }
+def get_config_params(config_file_path):
+    
+    round_names = get_round_names(config_file_path)
+    round_params = {}
+    for ro in round_names:
+        compute_param = {
+            'exp_path': (
+                summary_csv_path,[
+                    ('00BuildExperiment', 'base_dir_save'),
+                    ('00BuildExperiment.round_%s' % ro, 'name')
+                    ]
+                ),
+        }
+        rp = compute_workflow_params(config_file_path, compute_param)
+        round_params[ro] = rp
+    return round_params
 
 
 def main():
@@ -44,13 +61,9 @@ def main():
     parser.add_argument("--config")
     args = parser.parse_args()
 
-    config = configparser.ConfigParser()
-    config.read(args.config)
-
-    kwargs = conf_to_dict(config)
-    print(kwargs)
-
-    flow.run(parameters=kwargs)
+    r_params = get_config_params(args.config)
+    for ro, kwargs in r_params.items():
+        flow.run(parameters = kwargs)
 
 
 if __name__ == "__main__":
