6aa603739aa09a568e859b2f4c6f42694125c42c
Enrico Tagliavini <enrico.tagliavini@fmi.ch>
2023-03-03 11:12:13 +0100

    migrate 03_run_nuc_link.py to new config parser
    and rename a few parameters for consistency

--- scripts/prefect/03_run_nuc_link.py
+++ scripts/prefect/03_run_nuc_link.py
@@ -7,6 +7,14 @@ from prefect import Flow, Parameter, task, unmapped
 from prefect.executors import LocalDaskExecutor
 from prefect.run_configs import LocalRun
 
+from scmultiplex.config import (
+    commasplit,
+    compute_workflow_params,
+    get_round_names,
+    get_workflow_params,
+    parse_spacing,
+    summary_csv_path,
+)
 from scmultiplex.linking.NucleiLinking import link_nuclei
 
 
@@ -54,11 +62,11 @@ with Flow(
     "Nuclei-Linking", executor=LocalDaskExecutor(), run_config=LocalRun()
 ) as flow:
     rx_name = Parameter("RX_name", default="R1")
-    r0_csv = Parameter("R0_csv", default="/path/to/r0/summary.csv")
-    rx_csv = Parameter("RX_csv", default="/path/to/r1/summary.csv")
+    r0_csv = Parameter("R0_path", default="/path/to/r0/summary.csv")
+    rx_csv = Parameter("RX_path", default="/path/to/r1/summary.csv")
     excluded_plates = Parameter("excluded_plates", default=[])
     excluded_wells = Parameter("excluded_wells", default=[])
-    seg_name = Parameter("seg_name", default="NUC_SEG3D_220523")
+    nuc_ending = Parameter("nuc_ending", default="NUC_SEG3D_220523")
     ovr_channel = Parameter("ovr_channel", "C01")
     spacing = Parameter("spacing", default=[3.0, 1.0, 1.0])
 
@@ -70,38 +78,76 @@ with Flow(
     link_nuclei_task.map(
         r0_organoids,
         unmapped(ovr_channel),
-        unmapped(seg_name),
+        unmapped(nuc_ending),
         unmapped(rx_name),
         unmapped(RX),
         unmapped(spacing[-1] / spacing[0]),
     )
 
-
-def conf_to_dict(config):
-    return {
-        "R0_csv": config["DEFAULT"]["R0_csv"],
-        "RX_csv": config["DEFAULT"]["RX_csv"],
-        "RX_name": config["DEFAULT"]["RX_name"],
-        "excluded_plates": config["DEFAULT"]["excluded_plates"].split(","),
-        "excluded_wells": config["DEFAULT"]["excluded_wells"].split(","),
-        "seg_name": config["DEFAULT"]["seg_name"],
-        "ovr_channel": config["DEFAULT"]["ovr_channel"],
-        "spacing": [float(s) for s in config["DEFAULT"]["spacing"].split(",")],
-    }
-
+def get_config_params(config_file_path):
+    
+    round_names = get_round_names(config_file_path)
+    if len(round_names) < 2:
+        raise RuntimeError('At least two rounds are required to perform organoid linking')
+    rounds_tobelinked = round_names[1:]
+    config_params = {
+        'ovr_channel':     ('01FeatureExtraction', 'ovr_channel'),
+        }
+    common_params = get_workflow_params(config_file_path, config_params)
+    
+    compute_param = {
+        'excluded_plates': (
+            commasplit,[
+                ('01FeatureExtraction', 'excluded_plates')
+                ]
+            ),
+        'excluded_wells': (
+            commasplit,[
+                ('01FeatureExtraction', 'excluded_wells')
+                ]
+            ),
+        'spacing': (
+            parse_spacing,[
+                ('01FeatureExtraction', 'spacing')
+                ]
+            ),
+        'R0_path': (
+                summary_csv_path,[
+                    ('00BuildExperiment', 'base_dir_save'),
+                    ('00BuildExperiment.round_%s' % round_names[0], 'name')
+                    ]
+                ),
+        }
+    common_params.update(compute_workflow_params(config_file_path, compute_param))
+    
+    round_tobelinked_params = {}
+    for ro in rounds_tobelinked:
+        rp = common_params.copy()
+        rp['RX_name'] = ro
+        config_params = {
+            'nuc_ending':           ('00BuildExperiment.round_%s' % ro, 'nuc_ending'),
+            }
+        rp.update(get_workflow_params(config_file_path, config_params))
+        compute_param = {
+            'RX_path': (
+                summary_csv_path,[
+                    ('00BuildExperiment', 'base_dir_save'),
+                    ('00BuildExperiment.round_%s' % ro, 'name')
+                    ]
+                ),
+        }
+        rp.update(compute_workflow_params(config_file_path, compute_param))
+        round_tobelinked_params[ro] = rp
+    return round_tobelinked_params
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config")
     args = parser.parse_args()
 
-    config = configparser.ConfigParser()
-    config.read(args.config)
-
-    kwargs = conf_to_dict(config)
-    print(kwargs)
-
-    flow.run(parameters=kwargs)
+    r_params = get_config_params(args.config)
+    for ro, kwargs in r_params.items():
+        flow.run(parameters = kwargs)
 
 
 if __name__ == "__main__":
