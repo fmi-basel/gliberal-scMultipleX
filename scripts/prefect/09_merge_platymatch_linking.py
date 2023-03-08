a7da45514af01bf1237af878824b3e02945096cb
nrepina <nicole.repina@gmail.com>
2023-03-08 15:12:20 +0100

    Merge prefect task 11 into task 09.

--- scripts/prefect/09_merge_platymatch_linking.py
+++ scripts/prefect/09_merge_platymatch_linking.py
@@ -11,7 +11,10 @@ from scmultiplex.config import (
     get_round_names,
     summary_csv_path,
 )
-from scmultiplex.utils.accumulate_utils import merge_platymatch_linking
+from scmultiplex.utils.accumulate_utils import (
+    merge_platymatch_linking,
+    write_nuclear_linking_over_multiplexing_rounds
+)
 
 
 @task()
@@ -26,32 +29,56 @@ def merge_platymatch_linking_task(exp):
     merge_platymatch_linking(exp)
 
 
+@task()
+def write_nuclear_linking_over_multiplexing_rounds_task(
+    round_names, round_summary_csvs
+):
+    write_nuclear_linking_over_multiplexing_rounds(round_names, round_summary_csvs)
+
+
 with Flow(
-    "Merge PlatyMatch Linking",
+    "Merge Nuclear Linking",
     executor=LocalDaskExecutor(),
     run_config=LocalRun(),
 ) as flow:
     exp_path = Parameter("exp_path", default="/path/to/exp/summary.csv")
+    round_names = Parameter("round_names", default=["R0", "R1", "R2"])
+    round_summary_csv = Parameter("round_summary_csv")
 
     exp = load_experiment(exp_path)
 
     save_platymatch_linking = merge_platymatch_linking_task(exp)
 
+    nuc_ovr_mpx_rounds = write_nuclear_linking_over_multiplexing_rounds_task(
+        round_names,
+        round_summary_csv,
+    )
+
 
 def get_config_params(config_file_path):
-    
+
     round_names = get_round_names(config_file_path)
-    first_round = round_names[0]
-    # this script only need the first round
-    compute_param = {
-        'exp_path': (
-            summary_csv_path,[
-                ('00BuildExperiment', 'base_dir_save'),
-                ('00BuildExperiment.round_%s' % first_round, 'name')
+
+    if len(round_names) < 2:
+        raise RuntimeError('At least two rounds are required to perform organoid linking')
+
+    # initialize dictionary that contains list of summary.csv for each round
+    rp = {'round_names': round_names, 'round_summary_csv': []}
+
+    for ro in round_names:
+        compute_param = {
+            'RX_path': (
+                summary_csv_path, [
+                    ('00BuildExperiment', 'base_dir_save'),
+                    ('00BuildExperiment.round_%s' % ro, 'name')
                 ]
             ),
-    }
-    rp = compute_workflow_params(config_file_path, compute_param)
+        }
+        rscsv = compute_workflow_params(config_file_path, compute_param)['RX_path']
+        rp['round_summary_csv'].append(rscsv)
+
+    rp['exp_path'] = rp['round_summary_csv'][0] # select summary_csv of first round (R0)
+
     return rp
 
 
