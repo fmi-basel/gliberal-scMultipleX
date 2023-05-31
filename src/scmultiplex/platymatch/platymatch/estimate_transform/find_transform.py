f9e54b2a2da699ea47fabed94eef7c7793be63a9
Enrico Tagliavini <enrico.tagliavini@fmi.ch>
2023-05-31 16:06:39 +0200

    update platymatch to latest version

--- src/scmultiplex/platymatch/platymatch/estimate_transform/find_transform.py
+++ src/scmultiplex/platymatch/platymatch/estimate_transform/find_transform.py
@@ -1,47 +1,45 @@
 import numpy as np
-from numpy import inf
 from scipy.spatial import distance_matrix
-
+from numpy import inf
 
 def get_U(k):
     """
     k = N x N matrix
     """
-    log_k = np.log(k)
-    log_k[log_k == -inf] = 0
-    return k * log_k
-
-
-def get_tps_transform(moving, fixed, reg=0.0):
+    log_k=np.log(k)
+    log_k[log_k==-inf] = 0
+    return k*log_k
+    
+            
+def get_tps_transform(moving, fixed, reg = 0.0):
     """
     :param moving: point cloud N x 3 [x, y, z]
     :param fixed: point cloud N x 3
     """
-
+    
     N = moving.shape[0]
     # Solving A x = B
     # Create `A`
-    A = np.zeros((N + 4, N + 4))
+    A = np.zeros((N+4, N+4))
     k = distance_matrix(moving, moving)
-    K = get_U(k) + reg * np.identity(N)
+    K = get_U(k) + reg*np.identity(N)
     A[:N, :N] = K
     extra_one_col = np.ones((N, 1))
-    P = np.hstack((extra_one_col, moving))  # N x 4
-    PT = P.transpose()  # 4 x N
-    A[:N, N:] = P  # N x 4
-    A[N:, :N] = PT  # 4 x N
+    P = np.hstack((extra_one_col, moving)) # N x 4 
+    PT = P.transpose() # 4 x N
+    A[:N, N:] = P # N x 4
+    A[N:, :N] = PT # 4 x N 
     # Create `B`
-    B = np.zeros((N + 4, 3))
+    B = np.zeros((N+4, 3))
     Bx, By, Bz = B[:, 0:1], B[:, 1:2], B[:, 2:3]
-    Bx[:N, :] = fixed[:, 0:1]  # N x 1
-    By[:N, :] = fixed[:, 1:2]  # N x 1
-    Bz[:N, :] = fixed[:, 2:3]  # N x 1
+    Bx[:N, :] = fixed[:, 0:1] # N x 1 
+    By[:N, :] = fixed[:, 1:2] # N x 1
+    Bz[:N, :] = fixed[:, 2:3] # N x 1
     # find solution
-    w_a_x = np.matmul(np.linalg.inv(A), Bx)  # N x 1
-    w_a_y = np.matmul(np.linalg.inv(A), By)  # N x 1
-    w_a_z = np.matmul(np.linalg.inv(A), Bz)  # N x 1
+    w_a_x= np.matmul(np.linalg.inv(A), Bx) # N x 1
+    w_a_y= np.matmul(np.linalg.inv(A), By) # N x 1
+    w_a_z= np.matmul(np.linalg.inv(A), Bz) # N x 1
     return w_a_x, w_a_y, w_a_z
-
 
 def get_affine_transform(moving, fixed, with_ones=False):
     """
@@ -50,7 +48,7 @@ def get_affine_transform(moving, fixed, with_ones=False):
     :param with_ones: False
     :return: 4 x 4 affine matrix
     """
-    if with_ones:
+    if (with_ones):
         pass
     else:
         extra_one_row = np.ones((1, moving.shape[1]))
@@ -94,12 +92,10 @@ def get_similar_transform(moving, fixed):
     Szy = np.sum(Pz * Yy)
     Szz = np.sum(Pz * Yz)
 
-    Nmatrix = [
-        [Sxx + Syy + Szz, Syz - Szy, -Sxz + Szx, Sxy - Syx],
-        [-Szy + Syz, Sxx - Szz - Syy, Sxy + Syx, Sxz + Szx],
-        [Szx - Sxz, Syx + Sxy, Syy - Szz - Sxx, Syz + Szy],
-        [-Syx + Sxy, Szx + Sxz, Szy + Syz, Szz - Syy - Sxx],
-    ]
+    Nmatrix = [[Sxx + Syy + Szz, Syz - Szy, -Sxz + Szx, Sxy - Syx],
+               [-Szy + Syz, Sxx - Szz - Syy, Sxy + Syx, Sxz + Szx],
+               [Szx - Sxz, Syx + Sxy, Syy - Szz - Sxx, Syz + Szy],
+               [-Syx + Sxy, Szx + Sxz, Szy + Syz, Szz - Syy - Sxx]]
 
     [V, D] = np.linalg.eig(Nmatrix)  # TODO values, vectors
     # optimal quarternion vector corresponds to the largest eigen value
@@ -113,14 +109,15 @@ def get_similar_transform(moving, fixed):
     q2 = q[2]
     q3 = q[3]
 
-    Qbar = [
-        [q0, -q1, -q2, -q3],
-        [q1, q0, q3, -q2],
-        [q2, -q3, q0, q1],
-        [q3, q2, -q1, q0],
-    ]
+    Qbar = [[q0, -q1, -q2, -q3],
+            [q1, q0, q3, -q2],
+            [q2, -q3, q0, q1],
+            [q3, q2, -q1, q0]]
 
-    Q = [[q0, -q1, -q2, -q3], [q1, q0, -q3, q2], [q2, q3, q0, -q1], [q3, -q2, q1, q0]]
+    Q = [[q0, -q1, -q2, -q3],
+         [q1, q0, -q3, q2],
+         [q2, q3, q0, -q1],
+         [q3, -q2, q1, q0]]
 
     R = np.matmul(np.transpose(Qbar), Q)
     R = R[1:, 1:]
---
+++ src/scmultiplex/platymatch/platymatch/estimate_transform/perform_icp.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.spatial import distance_matrix
+from platymatch.estimate_transform.find_transform import get_affine_transform, get_similar_transform
+from platymatch.estimate_transform.apply_transform import apply_affine_transform
+from platymatch.utils.utils import get_error
+
+def perform_icp(moving, fixed, icp_iterations = 50, transform='Affine'):
+    if moving.shape[0] == 4:  # 4 x N
+        moving = moving[:3, :] # 3 x N
+    if fixed.shape[0] == 4:  # 4 x N
+        fixed = fixed[:3, :] # 3 x N
+
+    if transform == 'Affine':
+        get_transform = get_affine_transform
+        apply_transform = apply_affine_transform
+    elif transform == 'Similar':
+        get_transform = get_similar_transform
+        apply_transform = apply_similar_transform
+        
+    A_icp = np.identity(4)
+    residuals = np.zeros((icp_iterations))
+    for i in range(icp_iterations):
+        cost_matrix=distance_matrix(moving.transpose(), fixed.transpose()) # (N x 3, N x 3)
+        i2=np.argmin(cost_matrix, 1)
+        A_est=get_transform(moving, fixed[:, i2])
+        moving = apply_transform(moving, A_est)
+        residuals[i] = get_error(moving, fixed[:,i2])
+        A_icp=np.matmul(A_est, A_icp)
+    return A_icp, residuals
