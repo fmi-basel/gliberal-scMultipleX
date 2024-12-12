840f7d7f695284486346bd5adb35e2492d1ac858
jluethi <joel.luethi@uzh.ch>
2024-12-12 17:41:15 +0100

    Add docs_link to manifest

--- src/scmultiplex/dev/create_manifest.py
+++ src/scmultiplex/dev/create_manifest.py
@@ -5,4 +5,8 @@ from fractal_tasks_core.dev.create_manifest import create_manifest
 if __name__ == "__main__":
     PACKAGE = "scmultiplex"
     AUTHORS = "Nicole Repina, Enrico Tagliavini, Tim-Oliver Buchholz, Joel Luethi"
-    create_manifest(package=PACKAGE, authors=AUTHORS)
+    docs_link = "https://github.com/fmi-basel/gliberal-scMultipleX"
+    if docs_link:
+        create_manifest(package=PACKAGE, authors=AUTHORS, docs_link=docs_link)
+    else:
+        create_manifest(package=PACKAGE, authors=AUTHORS)
